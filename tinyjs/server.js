e267607858aa1279308d979696d3e5585ae9235e
JANE <JANE@unknown>
2016-01-21 06:34:04 +0000

    security

--- tinyjs/server.js
+++ tinyjs/server.js
@@ -14,7 +14,7 @@ var options = xsjs.extend({
 options = xsjs.extend(options, xsenv.getServices({ hana: {tag: "hana"} }));
 
 // configure UAA
-//options = xsjs.extend(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+options = xsjs.extend(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
 
 // start server
 xsjs(options).listen(port);
